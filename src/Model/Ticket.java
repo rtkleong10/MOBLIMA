7d02b92af0e1ea3664962cafdb07cd823afb348f
over_ <over_@DESKTOP-ULAQC4E>
2019-10-30 11:34:40 +0800

    booking, bookingApp, showtime, ticket added

---
+++ src/Model/Ticket.java
@@ -0,0 +1,22 @@
+package Model;
+
+import java.io.Serializable;
+
+public class Ticket implements Serializable {
+	private int row;
+	private int col;
+	
+	public Ticket(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	
+
+}
