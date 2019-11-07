b2502a4a157ad39f0607debaf19e334c436f75b6
over_ <over_@DESKTOP-ULAQC4E>
2019-11-07 16:46:45 +0800

    booking finally!!

--- src/Controller/BookingApp.java
+++ src/Controller/BookingApp.java
@@ -32,18 +32,20 @@ public class BookingApp {
 		Movie movie1 = new Movie("Spider-Man Far From Home", "Peter Parker's life", "Jon Watt", 
 				movie1Cast, ReleaseRating.PG13,
 				MovieType.SCIFI);
-		//dummy cinema
-		boolean[][] layout1 = {{false, true, true, false, true, true,false},
-						  		{false, true,true, false, true, true, false},
-						  		{false, true, true, false, true, true, false},
-						  		{false, true, true, false, true, true, false},
-						  		{false, true, true, false, true, true, false}};
 		
-		boolean[][] layout2 = {{false, true, true, false},
-		  		{false, true,true, false},
-		  		{false, true, true, false},
-		  		{false, true, true, false},
-		  		{false, true, true, false}};
+		boolean layout[][]=new boolean[11][19];
+        for(int i=0;i<11;i++){
+			
+			for(int j=0;j<19;j++)
+			{
+			if(j==4||j==14||i==6)
+			layout[i][j]=false;
+			
+			else
+			layout[i][j]=true;
+			}
+			
+        }
 		//generate showtimes schedule
 		int [] minute = {0,0,15,30,0,45} ;
 		int [] hour = {9, 10, 11, 12, 13, 14, 15, 16, 17};
@@ -53,7 +55,7 @@ public class BookingApp {
 		//duration cannot be float??
 		for (int i=0 ; i< MAX_SHOWTIMES; i++) {
 			schedule = LocalDateTime.of(2019, 12, i+1, hour[i%(hour.length)],minute[i%(minute.length)]);
-			show= new ShowTime( layout1, schedule ,Duration.ofMinutes(120), movie1 );
+			show= new ShowTime( layout, schedule ,Duration.ofMinutes(120), movie1 );
 			shows.add(show);
 		}
 		//moviegoer
@@ -62,17 +64,9 @@ public class BookingApp {
 		for (int i = 0; i < names.length; i++) {
 		    movieGoers[i] = new MovieGoer("nanyoci", names[i],null,null,null,null);
 		}
-		//booking data
-		boolean[][] book1 = {{false, false,false, false, false,false, false},
-		  		{false, false,false, false, false,false, false},
-		  		{false, false,false, false, false,false, false},
-		  		{false, false,false, false, false,false, false},
-		  		{false, false,false, false, true,true, false}};
-		boolean[][] book2 = {{false, true, true, false},
-		  		{false, true,true, false},
-		  		{false, true, true, false},
-		  		{false, true, true, false},
-		  		{false, true, true, false}};
+		
+		
+		
 		
 		String movieShowTime = "MovieShowTimes";
 		writeSerializedObject(movieShowTime, shows);
@@ -85,19 +79,20 @@ public class BookingApp {
 		displaySeat(readShow.get(0));
 		do {
 			book3=getSeat(readShow.get(0));
-			for(int j=0; j<book3.length; j++) {
+			
+			/*for(int j=0; j<book3.length; j++) {
 				for(int k=0 ; k<book3[j].length; k++) {
 					System.out.print(book3[j][k] + " ");
 				}
 				System.out.println("\n");
-			}
+			}*/
 			if(readShow.get(0).checkAvail( book3)) {
 				readShow.get(0).createBooking("123456",movieGoers[0], book3, 5.0);
 				System.out.println("Transaction ID: " +"123456"+" successful");
 				done = true;
 			}
 			else {
-				System.out.println("Seat is taken");
+				System.out.println("Seat is unavailable");
 			}
 		}while(!done);
 		
@@ -123,57 +118,72 @@ public class BookingApp {
 			for (int i=0; i<input.length; i++) {
 				row = input[i].charAt(0)-'A';
 				col = Integer.parseInt(input[i].substring(1) );
-				selectedSeat[row][col] = true;
+				selectedSeat[row][col-1] = true;
 		}
+		
+		for (int i = 0; i < selectedSeat.length; i++) {
+			for (int j = 0; j < selectedSeat[i].length; j++) {
+				System.out.print(selectedSeat[i][j] + " ");
+			}
+			
+			System.out.println("");
+		}	
 			
 		return selectedSeat;
 	}
-	
 	
 	public static  void displaySeat (ShowTime show) {
 		char row= 'A';
 		int col =1;
 		SeatStatus[][] availSeat = show.getSeatAvailabilities();
 		
-		for (int i =0; i< (3*(availSeat[0].length)/2)-2; i++)
+		for (int i = 0; i < availSeat.length; i++) {
+			for (int j = 0; j < availSeat[i].length; j++) {
+				System.out.print(availSeat[i][j] + " ");
+			}
+			
+			System.out.println("");
+		}
+		
+		for (int i =0; i< (5*(availSeat[0].length)/2)-2; i++)
 			System.out.print(" ");
 		System.out.println("SCREEN");
 		
-		for (int i =0; i< 3*(availSeat[0].length)+2; i++)
+		for (int i =0; i< 5*(availSeat[0].length)+4; i++)
 			System.out.print("-");
 		System.out.println();
-		System.out.print(" ");
+		System.out.print("  ");
 		
 		for (int i =0; i< (availSeat[0].length); i++)
 		{
-			if(availSeat[0][i] != SeatStatus.NO_SEAT) {
-				System.out.print(" "+col+" ");	
-				col++;
-			}
-			else if(availSeat[0][i] == SeatStatus.NO_SEAT) {
-				System.out.print("   ");
-			}
+			
+				if(i<10)
+					System.out.print("  "+col+"  ");	
+				else
+					System.out.print(" "+col+"  ");	
+			
+			col++;
 		}
 		System.out.println();	
-		for (int i =0; i< 3*(availSeat[0].length)+2; i++)
+		for (int i =0; i< 5*(availSeat[0].length)+4; i++)
 			System.out.print("-");
 		
 		System.out.println();
 		for (SeatStatus[] i : availSeat)
 		{
-			System.out.print(row );
+			System.out.print(row+" " );
 			
 		   for (SeatStatus j : i)
 		   {
 		        if (j == SeatStatus.TAKEN) 
-		   		System.out.print( "[x]");
+		   		System.out.print( "[ x ]");
 		   	else if(j== SeatStatus.EMPTY)
-		   		System.out.print( "[ ]");
+		   		System.out.print( "[   ]");
 		   	else if(j== SeatStatus.NO_SEAT)
-		   		System.out.print( "   ");
+		   		System.out.print( "     ");
 		   }
-		   System.out.print(row);
-		  row++;
+				System.out.print(" "+row );
+		   		row++;
 		   System.out.println();
 		   if(row == availSeat.length) {
 			   System.out.print(" ");
@@ -181,27 +191,30 @@ public class BookingApp {
 		   }
 		}
 		
-		for (int i =0; i< 3*(availSeat[0].length)+2; i++)
+		for (int i =0; i< 5*(availSeat[0].length)+4; i++)
 			System.out.print("-");
 		System.out.println();
 		
 		col =1;
-		System.out.print(" ");
+		System.out.print("  ");
+		
 		for (int i =0; i< (availSeat[0].length); i++)
 		{
-			if(availSeat[0][i] != SeatStatus.NO_SEAT) {
-				System.out.print(" "+col+" ");	
-				col++;
-			}
-			else if(availSeat[0][i] == SeatStatus.NO_SEAT) {
-				System.out.print("   ");
-			}
+			
+				if(i<10)
+					System.out.print("  "+col+"  ");	
+				else
+					System.out.print(" "+col+"  ");	
+			
+			col++;
 		}
+		
+		
 		System.out.println();	
-		for (int i =0; i< 3*(availSeat[0].length)+2; i++)
+		for (int i =0; i< 5*(availSeat[0].length)+4; i++)
 			System.out.print("-");
 		System.out.println();
-		for (int i =0; i< (3*(availSeat[0].length)/2)-3; i++)
+		for (int i =0; i< (5*(availSeat[0].length)/2)-3; i++)
 			System.out.print(" ");
 		System.out.println("ENTRANCE");
 		
