5d279603fb113dd33378e4c79417ba9ca6bc2ec0
Rajeswari <Rajeswari@LAPTOP-2479EIKI>
2019-11-14 22:24:09 +0800

    Added Javadocs for Cinema,CinemaClass, and Cineplex

--- src/model/CinemaClass.java
+++ src/model/CinemaClass.java
@@ -1,15 +1,29 @@
+/**
+ *  Cinema classes that can be used
+ */
 package model;
-
 public enum CinemaClass implements LabelledItem {
+	/**
+     * Normal cinema class
+     */
 	NORMAL("Normal"),
+	/**
+     * Platinum Movie Suite cinema class
+     */
 	PLATINUM_MOVIE_SUITE("Platinum Movie Suite");
 	
 	String label;
-	
+	/**
+     * Constructor for each enum
+     * @param label the name of the cinema class
+     */
 	private CinemaClass(String label) {
 		this.label = label;
 	}
-	
+	/** 
+     * Returns the label parameter of the enum class
+     * @return the label of the enum
+     */
 	public String getLabel() {
 		return label;
 	}
